7acd8d4fb918dbf7f1a8033b9bc08d243ff4dcf1
kema-ray <rachelkemuma99@gmail.com>
2022-11-04 14:21:37 +0300

    create serializer and urls file

--- drf/settings.py
+++ drf/settings.py
@@ -120,6 +120,14 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        # 'rest_framework.authentication.BasicAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
+        'knox.auth.TokenAuthentication',
+    ]
+}
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
